cb5c71ddcb8d8a6c605661e9fcc2ed37c6ea58dd
smylere <caesitas@gmail.com>
2020-07-20 22:31:06 +0900

    주석수정

--- 2-1/answer_2-1.sql
+++ 2-1/answer_2-1.sql
@@ -19,19 +19,19 @@ WITH TMP AS
 			WHERE 
 				MENU_NM NOT IN ('login','logout')
 			)B
-		) CORE 																				-- 곱집합 메뉴명/날짜 별도 계산
+		) CORE	-- 곱집합 메뉴명/날짜 별도 계산
 	)
 SELECT
 	TMP.DAY_NAME
-	,ROW_NUMBER() OVER(PARTITION BY TMP.DAY_NAME ORDER BY CNT DESC,TMP.MENU_NM ASC) RNK		-- RANK계산
+	,ROW_NUMBER() OVER(PARTITION BY TMP.DAY_NAME ORDER BY CNT DESC,TMP.MENU_NM ASC) RNK	-- RANK계산
 	,CASE 
 		WHEN SUB.CNT IS NOT NULL THEN TMP.MENU_NM
 		WHEN SUB.CNT IS NULL THEN '-'
-		END AS MENU_NM																		-- 메뉴명 체크(없을경우 '-')
+		END AS MENU_NM	-- 메뉴명 체크(없을경우 '-')
 	,CASE 
 		WHEN SUB.CNT IS NOT NULL THEN SUB.CNT 
 		WHEN SUB.CNT IS NULL THEN '-'
-		END AS CNT																			-- 메뉴 카운트(없을경우 '-')
+		END AS CNT	-- 메뉴 카운트(없을경우 '-')
 FROM 
 	TMP 
 LEFT JOIN
@@ -39,12 +39,12 @@ LEFT JOIN
 	SELECT 
 		DAYNAME(CAST(LOG_TKTM AS DATE)) AS DAY_NAME
 		,MENU_NM
-		,COUNT(MENU_NM) AS CNT 																-- 메뉴 진입 COUNT
+		,COUNT(MENU_NM) AS CNT	-- 메뉴 진입 COUNT
 	FROM 
 		KAKAOBANK.MENU_LOG ML 
 	WHERE
 		1=1
-		AND ML.MENU_NM NOT IN('login','logout')												-- LOGIN / LOGOUT 내역 제외
+		AND ML.MENU_NM NOT IN('login','logout')	-- LOGIN / LOGOUT 내역 제외
 	GROUP BY 
 		DAY_NAME
 		,MENU_NM
